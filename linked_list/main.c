d0bd23d0f2c1ae273ec2e847495b08e684583fa2
Richard Bannon <richard.bannon@mac.com>
2018-11-25 14:54:58 -0800

    Initial Makefile/IDE checkin

---
+++ linked_list/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Linked List test application...\n\n");
+
+    /* Unused args */
+    (void)argc;
+    (void)argv;
+        
+    return 0;
+}
